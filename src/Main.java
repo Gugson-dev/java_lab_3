78588a16180bb143ea3a3e31a5856ad8620c50a2
krzys <krzysztofmuller1@gmail.com>
2021-09-04 15:53:43 +0200

    ćw.1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        /* Przykłady wyświetlania tekstu w konsoli */
-        System.out.println("Witaj świecie!");
-        System.out.println("Po jest nasz pierwszy program w języku java");
-        System.out.println("Życzę" + " " + "wam " + "miłej nauki!");
-
-        System.out.printf("A %s zaczynamy z %d przykładem.\n", "więc", 1);
-
-        System.out.print("To jest jakiś");
-        System.out.print(" tekst");
-        System.out.print(" nie za długi.");
-
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+
 
     }
 }
