98cdf2018eb89996be39c0ea058b0c094ab8de38
cookiespooky <jula.rumi@gmail.com>
2021-11-21 19:47:50 +0100

    sd

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("sds");
         System.out.println("dsa");
+        System.out.println("dsa");
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
