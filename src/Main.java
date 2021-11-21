9d52e33c963b7a47cac946f1e8e4eb3e396c9ee5
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2021-11-21 20:08:42 +0100

    wypisanie tekstu

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("sds");
-        System.out.println("dsa");
-        System.out.println("dsa");
+        System.out.println("Witaj,");
+        System.out.printf("oto 3 sposoby na wypisanie tekstu: \n");
+        System.out.printf("Sposób %s jest taki\n",1);
+        System.out.println("Sposób 2 jest taki");
+        System.out.print("Oto sposób 3");
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
