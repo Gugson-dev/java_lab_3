aaac34a536da659c34d9902adc436b03cc348460
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2022-01-22 16:35:29 +0100

    ćw2, zad1

---
+++ ćw2/zad1.java
@@ -0,0 +1,17 @@
+public class zad1 {
+
+    public static void main(String[] args) {
+/*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+        int a = 1, b = 0 , c = -16;
+        String elo = "siema", nara = "nie", witam = "dzień dobry";
+        boolean tak = true, nie = false, moze = 5 > 4;
+        double ad = 1.5, bd = 15.3, cd = -16.12312;
+        char literka='j', inna = 'p', h = 'h';
+
+        System.out.println("Zmienne int:\n- " + a + "\n- " + b + "\n- " + c);
+        System.out.println("Zmienne string:\n- " + elo + "\n- " + nara + "\n- " + witam);
+        System.out.println("Zmienne boolean:\n- " + tak + "\n- " + nie + "\n- " + moze);
+        System.out.println("Zmienne float:\n- " + ad + "\n- " + bd+ "\n- " + cd);
+        System.out.println("Zmienne char:\n- " + literka + "\n- " + inna + "\n- " + h);
+    }
+}
