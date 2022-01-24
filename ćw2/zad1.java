f5cec0459e995dac9de4bbb1c77e1031d38b7d54
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2022-01-24 23:41:00 +0100

    Zrobione

--- ćw2/zad1.java
+++ ćw2/zad1.java
@@ -1,17 +1,46 @@
+import java.util.Scanner;
+
 public class zad1 {
 
-    public static void main(String[] args) {
-/*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
-        int a = 1, b = 0 , c = -16;
-        String elo = "siema", nara = "pa", witam = "dzień dobry";
-        boolean tak = true, nie = false, moze = 5 > 4;
-        double ad = 1.5, bd = 15.3, cd = -16.12312;
-        char literka='j', inna = 'p', h = 'h';
+    /*
+    
+    Zadanie praktycznie identyczne co ćw1/zadanie 1, dlatego dodałem metodę żeby kod się trochę różnił
 
-        System.out.println("Zmienne int:\n- " + a + "\n- " + b + "\n- " + c);
-        System.out.println("Zmienne string:\n- " + elo + "\n- " + nara + "\n- " + witam);
-        System.out.println("Zmienne boolean:\n- " + tak + "\n- " + nie + "\n- " + moze);
-        System.out.println("Zmienne double:\n- " + ad + "\n- " + bd+ "\n- " + cd);
-        System.out.println("Zmienne char:\n- " + literka + "\n- " + inna + "\n- " + h);
+    */
+
+    public static int skroc(int indeks, int ile){
+
+        String ost_cyfry = Integer.toString(indeks);
+        int podzielnik = Integer.parseInt(ost_cyfry.substring(ost_cyfry.length() - ile));
+        return podzielnik;
+
+    }
+
+    public static void main(String[] args) {
+        /* zad.1 Napisz program który będzie iterował od 100 do 0,
+         *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
+         * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Podaj swój numer indeksu: ");
+        int indeks = input.nextInt();
+        input.close();
+
+        int x = 100;
+        int ostatnia = skroc(indeks, 1);
+        int ostatnie2 = skroc(indeks, 2);
+
+        do {
+          if (ostatnia == 0) {
+              if (x % ostatnie2 == 0) {
+                System.out.printf("Liczba %d podzielna przez %d\n", x, ostatnie2);
+              }
+          }
+          else if (x % ostatnia == 0) {
+                System.out.printf("Liczba %d podzielna przez %d\n", x, ostatnia);
+          }
+          x--;
+        } while (x != 0);
     }
 }
--- ćw2/zad2.java
+++ ćw2/zad2.java
@@ -1,27 +1,28 @@
+import java.util.Scanner;
+
 public class zad2 {
 
     public static void main(String[] args) {
-        /*
-        *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-        *    mnożenia, dziealenia i modulo na zmiennych:
-        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        */
-        int liczbaA = 3, liczbaB = 7;
-        double liczbaX = 3.5, liczbaY = 1.5;
-        
-        System.out.println("===============\n      INT\n===============");
-        System.out.printf("Wynik dodawania %d i %d: " + (liczbaA + liczbaB) + "\n",liczbaA,liczbaB);
-        System.out.printf("Wynik odejmowania %d i %d: " + (liczbaA - liczbaB) + "\n",liczbaA,liczbaB);
-        System.out.printf("Wynik mnożenia %d i %d: " + (liczbaA * liczbaB) + "\n",liczbaA,liczbaB);
-        System.out.printf("Wynik dzielenia %d i %d: " + (liczbaA / liczbaB) + "\n",liczbaA,liczbaB);
-        System.out.printf("Wynik modulo %d i %d: " + (liczbaA % liczbaB) + "\n",liczbaA,liczbaB);
-        System.out.println("===============\n    DOUBLE\n===============");
-        System.out.printf("Wynik dodawania %.2f i %.2f: " + (liczbaX + liczbaY) + "\n",liczbaX,liczbaY);
-        System.out.printf("Wynik odejmowania %.2f i %.2f: " + (liczbaX - liczbaY) + "\n",liczbaX,liczbaY);
-        System.out.printf("Wynik mnożenia %.2f i %.2f: " + (liczbaX * liczbaY) + "\n",liczbaX,liczbaY);
-        System.out.printf("Wynik dzielenia %.2f i %.2f: " + (liczbaX / liczbaY) + "\n",liczbaX,liczbaY);
-        System.out.printf("Wynik modulo %.2f i %.2f: " + (liczbaX % liczbaY) + "\n",liczbaX,liczbaY);
-        System.out.print("===============");
+        /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
+         * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
+         * za pomocą pętli forEach powiększone o 11 */
+
+         Scanner input = new Scanner(System.in);
+         int[] liczby = new int[5];
+
+         System.out.println("Podaj kolejno 5 liczb:");
+         for (int i = 0; i < 5; i++) {
+            System.out.printf("%d. ", i+1);
+            liczby[i] = input.nextInt();
+         }
+
+         input.close();
+         int k = 1;
+
+         System.out.println("\nTwoje liczby zwiększone o 11 to:");
+         for (int l : liczby) {
+            System.out.printf("%d. %d\n", k, l+11);
+            k++;
+         }
     }
 }
