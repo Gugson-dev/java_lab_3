9ea49c37e1a8ef6b6f1b0da3c4dbedec059918a5
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2022-01-25 02:00:54 +0100

    Zrobione

--- ćw3/zad1.java
+++ ćw3/zad1.java
@@ -3,28 +3,29 @@ import java.util.Scanner;
 public class zad1 {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        /* zad.1 napisać program który przyjmuje jedną liczbę od użytkownika z klawiatury
+          i za pomocą switch case default ma określić czy:
+          a) ta liczba jest parzysta --> podpowiedz switch(a % 2) {
+          b) program który określi czy liczba jest podzielna przez 7
+         */
 
-        System.out.print("Podaj imie: ");
-        String imie = input.next();
+         Scanner input = new Scanner(System.in);
 
-        System.out.print("Podaj nazwisko: ");
-        String nazwisko = input.next();
+         System.out.print("Podaj liczbe aby sprawdzić czy jest podzielna przez 2 i 7: ");
+         int liczba = input.nextInt();
+         
+         input.close();
+         String dziel = (liczba % 7 == 0) ? "\nLiczba " + liczba + " jest podzielna przez 7" : "\nLiczba " + liczba + " nie jest podzielna przez 7";
 
-        System.out.print("Podaj wiek: ");
-        int wiek = input.nextInt();
-
-        System.out.print("Podaj numer indeksu: ");
-        int nrIndeksu = input.nextInt();
-
-        input.close();
-        
-        System.out.print(
-        "\nTwoje dane:" + 
-        "\nImię - " + imie + 
-        "\nNazwisko - " + nazwisko + 
-        "\nWiek - " + wiek + 
-        "\nNumer indeksu - " + nrIndeksu
-        );
+         switch (liczba % 2) {
+             case 0:
+                 System.out.printf("Liczba %d jest podzielna przez 2%s", liczba, dziel);
+                 
+                 break;
+         
+             default:
+                 System.out.printf("Liczba %d nie jest podzielna przez 2%s", liczba, dziel);
+                 break;
+         }
     }
 }
--- ćw3/zad2.java
+++ ćw3/zad2.java
@@ -1,28 +1,53 @@
-import java.util.Scanner;
-
 public class zad2 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        /* zad.2
+           a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
+            "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
+           b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
+         */
 
-        System.out.print("Podaj liczbe A: ");
-        int a = input.nextInt();
+        String wyraz = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int 
+        o = 0, 
+        p = 0, 
+        x = 0, 
+        z = 0, 
+        q = 0, 
+        inne = 0;
 
-        System.out.print("Podaj liczbe B: ");
-        int b = input.nextInt();
+        for (int i = 0; i < wyraz.length(); i++) {
+            switch (wyraz.charAt(i)) {
+                case 'o':
+                    o++;
+                    break;
 
-        input.close();
+                case 'p':
+                    p++;
+                    break;
 
-        int dodawanie = a + b;
-        int odejmowanie = a - b;
-        int mnozenie = a * b;
-        int dzielenie = a / b;
-        int modulo = a % b;
+                case 'x':
+                    x++;
+                    break;
 
-        System.out.println("\nDziałania na podanych liczbach:\n");
-        System.out.printf("%d + %d = " + dodawanie + "\n", a, b);
-        System.out.printf("%d - %d = " + odejmowanie + "\n", a, b);
-        System.out.printf("%d * %d = " + mnozenie + "\n", a, b);
-        System.out.printf("%d / %d = " + dzielenie + "\n", a, b);
-        System.out.printf("%d modulo %d = " + modulo + "\n", a, b);
+                case 'z':
+                    z++;
+                    break;
+
+                case 'q':
+                    q++;
+                    break;
+
+                default:
+                    inne++;
+                    break;
+            }
+        }
+        System.out.println("Podany wyraz ma w sobie:");
+        System.out.printf("Liter 'o' = %d\n", o);
+        System.out.printf("Liter 'p' = %d\n", p);
+        System.out.printf("Liter 'x' = %d\n", x);
+        System.out.printf("Liter 'z' = %d\n", z);
+        System.out.printf("Liter 'q' = %d\n", q);
+        System.out.printf("Innych liter = %d", inne);
     }
 }
